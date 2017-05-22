dc6cb043085210b81cfce230dcedbabc7a75bb48
DiegoPadillaRomero <diego.padilla.91@gmail.com>
2017-05-22 11:26:36 +0200

    Update RestaurarBD.ps1

--- RestaurarBD.ps1
+++ RestaurarBD.ps1
@@ -8,7 +8,7 @@ Invoke-WebRequest "http://webjobdatos.blob.core.windows.net/app-log/AdventureWor
 $sqlServer = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $sqlName
  
 $SqlRestore = "RESTORE DATABASE [AdventureWorksDW2014] FROM  DISK = N'C:\AdventureWorks\AdventureWorksDW2014.bak' WITH  FILE = 1,  MOVE N'AdventureWorksDW2014_Data' TO N'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\AdventureWorksDW2014_Data.mdf',  MOVE N'AdventureWorksDW2014_Log' TO N'C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\DATA\AdventureWorksDW2014_Log.ldf',  NOUNLOAD,  STATS = 5"
-$connString = "Server=$sqlname;integrated security= SSPI ;database=master;"
+$connString = "Server=$sqlname;integrated security= true ;database=master;"
 
 $Conn = New-Object ('System.data.sqlclient.sqlconnection') $connString
 
